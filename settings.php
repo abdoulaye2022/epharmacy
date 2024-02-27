dd95268b8daf7f860ca2e4ff92b1286a87fa31d5
Lucas Caissie <Lucasmcaissie@gmail.com>
2024-02-27 10:08:22 -0400

    le setting garde good now

--- settings.php
+++ settings.php
@@ -52,18 +52,18 @@ require_once("./controllers/ProfilController.php");
                             <div class="overlay-content">
                                 <div class="text-end p-3">
                                     <div class="p-0 ms-auto rounded-circle profile-photo-edit">
-                                        <input id="profile-foreground-img-file-input" type="file" class="profile-foreground-img-file-input">
+                                       
                                         <label for="profile-foreground-img-file-input" class="profile-photo-edit btn btn-light">
-                                            <i class="ri-image-edit-line align-bottom me-1"></i> Change Cover
+                                            
                                         </label>
                                     </div>
                                 </div>
                             </div>
                     </div>
 
-                    <div class="row">
+                    
                         <div class="col-xxl-3">
-                            <div class="card mt-n5">
+                        <div class="card mt-n5" style="margin-bottom: 70px;">
                                 <div class="card-body p-4">
                                     <div class="text-center">
                                         <div class="profile-user position-relative d-inline-block mx-auto  mb-4">
@@ -82,55 +82,9 @@ require_once("./controllers/ProfilController.php");
                                     </div>
                                 </div>
                             </div>
-                            <!--end card-->
-                            <!-- <div class="card">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center mb-4">
-                                        <div class="flex-grow-1">
-                                            <h5 class="card-title mb-0">Portfolio</h5>
-                                        </div>
-                                        <div class="flex-shrink-0">
-                                            <a href="javascript:void(0);" class="badge bg-light text-primary fs-12"><i class="ri-add-fill align-bottom me-1"></i> Add</a>
-                                        </div>
-                                    </div>
-                                    <div class="mb-3 d-flex">
-                                        <div class="avatar-xs d-block flex-shrink-0 me-3">
-                                            <span class="avatar-title rounded-circle fs-16 bg-dark text-light shadow">
-                                                <i class="ri-github-fill"></i>
-                                            </span>
-                                        </div>
-                                        <input type="email" class="form-control" id="gitUsername" placeholder="Username" value="@daveadame">
-                                    </div>
-                                    <div class="mb-3 d-flex">
-                                        <div class="avatar-xs d-block flex-shrink-0 me-3">
-                                            <span class="avatar-title rounded-circle fs-16 bg-primary shadow">
-                                                <i class="ri-global-fill"></i>
-                                            </span>
-                                        </div>
-                                        <input type="text" class="form-control" id="websiteInput" placeholder="www.example.com" value="www.velzon.com">
-                                    </div>
-                                    <div class="mb-3 d-flex">
-                                        <div class="avatar-xs d-block flex-shrink-0 me-3">
-                                            <span class="avatar-title rounded-circle fs-16 bg-success shadow">
-                                                <i class="ri-dribbble-fill"></i>
-                                            </span>
-                                        </div>
-                                        <input type="text" class="form-control" id="dribbleName" placeholder="Username" value="@dave_adame">
-                                    </div>
-                                    <div class="d-flex">
-                                        <div class="avatar-xs d-block flex-shrink-0 me-3">
-                                            <span class="avatar-title rounded-circle fs-16 bg-danger shadow">
-                                                <i class="ri-pinterest-fill"></i>
-                                            </span>
-                                        </div>
-                                        <input type="text" class="form-control" id="pinterestName" placeholder="Username" value="Advance Dave">
-                                    </div>
-                                </div>
-                            </div> -->
-                            <!--end card-->
                         </div>
                         <!--end col-->
-                        
+                        <!-- <div class="row"> -->
                             <div class="card mt-xxl-n5">
                                 <div class="card-header">
                                     <ul class="nav nav-tabs-custom rounded card-header-tabs border-bottom-0" role="tablist">
@@ -232,7 +186,7 @@ require_once("./controllers/ProfilController.php");
                                             <div class="mb-3">
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input" type="checkbox" id="darkModeToggle">
-                                                    <label class="form-check-label" for="darkModeToggle">Enable Dark Mode</label>
+                                                    <label class="form-check-label" for="darkModeToggle">Dark Mode</label>
                                                 </div>
                                             </div>
 
@@ -323,7 +277,7 @@ require_once("./controllers/ProfilController.php");
                             </div>
                         
                         <!--end col-->
-                    </div>
+                        <!-- </div> -->
                     <!--end row-->
 
                 </div>
